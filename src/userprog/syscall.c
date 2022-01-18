0c3f69984ee44975960e1e3eefaa05c7fef12b1e
Nehag <Neha2.Giri@live.uwe.ac.uk>
2022-01-18 23:29:29 +0000

    userprog: syscall: add write syscall for console.
    This change adds support to write on the console using putbuf.
    
    Signed-off-by: Nehag <Neha2.Giri@live.uwe.ac.uk>

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -19,10 +19,9 @@ syscall_handler (struct intr_frame *f UNUSED)
   struct thread *cur = thread_current ();
   int syscall_num;
   int *sp = f->esp;
-  
  
   syscall_num = *sp;
-  printf ("system call (%d)!\n",syscall_num);
+//  printf ("system call (%d)!\n",syscall_num);
   if (syscall_num == SYS_EXIT )
   {	/* Read the exit code */
 	  sp = (f->esp + 4);
@@ -30,4 +29,23 @@ syscall_handler (struct intr_frame *f UNUSED)
 	  cur->exit_code = *sp;
 	  thread_exit ();
   }
+
+  if (syscall_num == SYS_WRITE )
+  {
+	int fd;
+	char * buffer;
+	unsigned size;
+
+	sp = (f->esp + 4);
+	fd = *sp;
+        sp = (f->esp + 8);
+        buffer = *sp;
+	sp = (f->esp + 12);
+	size = *sp;
+	//printf("fd %d size %d buf %x\n", fd, size, buffer);
+	if(fd == 1)
+	{
+          putbuf(buffer, size);
+	}
+  }	  
 }
